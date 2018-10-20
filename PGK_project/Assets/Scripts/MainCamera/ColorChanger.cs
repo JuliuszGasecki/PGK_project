a18b4e79d8fde6d14a864acd4da794584bc7928c
Juliusz Gąsecki <julekg.97@gmail.com>
2018-10-20 21:03:42 +0200

    Lil update

--- PGK_project/Assets/Scripts/MainCamera/ColorChanger.cs
+++ PGK_project/Assets/Scripts/MainCamera/ColorChanger.cs
@@ -20,11 +20,14 @@ public class ColorChanger : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float t = Mathf.Sin(duration*Time.time);
-        int rand = Random.Range(1, 2);
-        if (rand == 1)
-            cam.backgroundColor = Color.Lerp(color1, color2, t);
-        else
-            cam.backgroundColor = Color.Lerp(color1, color3, t);
-
+        /* int rand = Random.Range(1, 3);
+         if (rand == 1)
+             cam.backgroundColor = Color.Lerp(color1, color2, t);
+         else if (rand == 2)
+             cam.backgroundColor = Color.Lerp(color2, color3, t);
+         else if (rand == 3)
+             cam.backgroundColor = Color.Lerp(color3, color1, t);
+             */
+        cam.backgroundColor = Color.Lerp(color1, color3, t);
     }
 }
