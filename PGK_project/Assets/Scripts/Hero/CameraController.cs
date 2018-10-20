eba561ebe44020c4b2d230c4058bd6460fef0421
Bartosz <bartekstep@gmail.com>
2018-10-20 03:28:56 +0200

    Testy skryptow
    Proste testy skryptow, nie działa kamera

---
+++ PGK_project/Assets/Scripts/Hero/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform FollowTarget;
+    public Vector3 TargetOffset;
+    public float MoveSpeed = 2f;
+
+    private Transform _myTransform;
+
+    private void Start()
+    {
+        // Cache camera transform
+        _myTransform = transform;
+    }
+
+    public void SetTarget(Transform aTransform)
+    {
+        FollowTarget = aTransform;
+    }
+
+    private void LateUpdate()
+    {
+        if (FollowTarget != null)
+            _myTransform.position = Vector3.Lerp(_myTransform.position, FollowTarget.position + TargetOffset, MoveSpeed * Time.deltaTime);
+    }
+}
