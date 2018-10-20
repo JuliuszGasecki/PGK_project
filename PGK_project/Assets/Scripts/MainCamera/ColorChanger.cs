e0c6c773f817fcc7bc0593df3fb790a8ab7bb0a1
Juliusz Gąsecki <julekg.97@gmail.com>
2018-10-21 01:46:03 +0200

    First drug

--- PGK_project/Assets/Scripts/MainCamera/ColorChanger.cs
+++ PGK_project/Assets/Scripts/MainCamera/ColorChanger.cs
@@ -3,12 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorChanger : MonoBehaviour {
-
-    public Color color1 = Color.blue;
-    public Color color2 = Color.red;
-    public Color color3 = Color.green;
     public float duration = 1.0f;
-    private int colorFlag = 0;
     public Camera cam;
 
 	// Use this for initialization
