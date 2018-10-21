5e9d9d63b910b92274d512e970ebbafe02d38e2b
Bartosz <bartekstep@gmail.com>
2018-10-21 18:44:04 +0200

    Dodana scena umierania

--- PGK_project/Assets/Scripts/DeathScene/DeathScene.cs
+++ PGK_project/Assets/Scripts/DeathScene/DeathScene.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 public class DeathScene : MonoBehaviour {
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
---
+++ PGK_project/Assets/Scripts/DeathScene/ReturnButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReturnButton : MonoBehaviour {
+    public void Return(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
