e464eaa192dd0dad4b67bc5fb604f026a6293dba
Bartosz <bartekstep@gmail.com>
2018-10-23 01:14:51 +0200

    Można pobierać

--- PGK_project/Assets/Scripts/Weapon/WeaponTest.cs
+++ PGK_project/Assets/Scripts/Weapon/WeaponTest.cs
@@ -9,7 +9,13 @@ public class WeaponTest : MonoBehaviour {
     private float timeUntilFire = 0;
     Transform firePoint;
     public GameObject bullet;
-	void Start () {
+    private float colliderOffset;
+    private float tempColliderOffset;
+    private Vector2 collider;
+    private bool isHit;
+	void Start ()
+	{
+	    collider = this.gameObject.GetComponent<CircleCollider2D>().offset;
 	}
 
     void Awake()
@@ -23,17 +29,40 @@ public class WeaponTest : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     { 
-	        if (Input.GetMouseButtonDown(0) && Time.time > timeUntilFire)
+	        if (Input.GetMouseButtonDown(0) && Time.time > timeUntilFire && this.GetComponent<Hero>().canShoot)
 	        {
 	            timeUntilFire = Time.time + 1 / fireRate;
 	            Shoot();
 	        }
+
+        if (Input.GetMouseButtonDown(0) && !this.GetComponent<Hero>().canShoot)
+        {
+            Attack();
+        }
 	   
 	}
+
+    void Attack()
+    {
+        colliderOffset = collider.y;
+        collider.y += 4.0f;
+        collider.y = colliderOffset;
+    }
 
     void Shoot()
     {
       
             Instantiate(bullet, firePoint.position, firePoint.rotation);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            isHit = false;
+        }
+
+        
     }
 }
