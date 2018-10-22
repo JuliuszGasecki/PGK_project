d1c43cad22794bd9ead7b78c4533e35a82233e5e
Bartosz <bartekstep@gmail.com>
2018-10-22 23:15:49 +0200

    jprd

---
+++ PGK_project/Assets/Scripts/Hero/ChangeSprite.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour {
+
+    private string spriteWeapon = "hero_weapon";
+    private string spriteHero = "hero_knife";
+    private SpriteRenderer spriteR;
+    private bool isChange;
+    void Start()
+    {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (isChange)
+            {
+                spriteR.sprite = Resources.Load<Sprite>(spriteWeapon);
+                isChange = false;
+            }
+            if (!isChange)
+            {
+                spriteR.sprite = Resources.Load<Sprite>(spriteHero);
+                isChange = true;
+            }
+        }
+    }
+}
