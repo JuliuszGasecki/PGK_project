af955eeb09a9d284f2e6071af480cef102d78adf
Juliusz Gąsecki <julekg.97@gmail.com>
2018-10-25 00:16:06 +0200

    Auto stash before merge of "master" and "origin/master"

--- PGK_project/Assets/Scripts/MainCamera/ColorChanger.cs
+++ PGK_project/Assets/Scripts/MainCamera/ColorChanger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorChanger : MonoBehaviour {
+    public DrugsTimer hero;
     public float duration = 1.0f;
     public Camera cam;
 
@@ -14,12 +15,12 @@ public class ColorChanger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        if(hero.tookDrug == true)
+        {
         float r = Mathf.Sin(duration*Time.time);
         float g = Mathf.Sin(duration*Time.time*3.14f);
         float b = Mathf.Cos(duration * Time.time+3);
-
         cam.backgroundColor = new Vector4(r, g , b , 0.5f );
-        
+        }  
     }
 }
