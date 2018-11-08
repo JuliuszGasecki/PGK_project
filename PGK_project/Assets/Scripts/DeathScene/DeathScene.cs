a2811c69edeccbcdcbb1303748b7555b5b917001
Bartosz <bartekstep@gmail.com>
2018-11-08 03:10:03 +0100

    DeathScene part 1.1

--- PGK_project/Assets/Scripts/DeathScene/DeathScene.cs
+++ PGK_project/Assets/Scripts/DeathScene/DeathScene.cs
@@ -11,17 +11,20 @@ public class DeathScene : MonoBehaviour
     public float temp;
     private Hero hero;
     private SpriteRenderer spriteR;
+    private Animator anim;
 
     void Start()
     {
         hero = GameObject.Find("Hero").GetComponent<Hero>();
         spriteR = GameObject.Find("Hero").GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         if (hero.isAlive() == false || hero.drugWithdrawal <= 0 || hero.poisoning > 40)
         {
+            anim.enabled = false;
             spriteR.sprite = Resources.Load<Sprite>(deadHero);
             Pause();
         }
