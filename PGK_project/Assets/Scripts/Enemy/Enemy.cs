0a4a19b98d7f138cf34cdbdf72f1995c64b7a86c
Marta <martadebecka1@wp.pl>
2018-11-17 21:13:26 +0100

    Usunięte niepotrzebne animacje i zakomentowane linijki powodujące błedy

--- PGK_project/Assets/Scripts/Enemy/Enemy.cs
+++ PGK_project/Assets/Scripts/Enemy/Enemy.cs
@@ -45,7 +45,7 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
-        anim = GetComponent<Animator>();
+       // anim = GetComponent<Animator>();
         go_forwad = true;
         currentState = statePATH;
         targetpathPoint = 0;
@@ -77,7 +77,7 @@ public class Enemy : MonoBehaviour
                 if (znaleziono_gracza())
                 {
 
-                    anim.SetBool("isWalking", true);
+                    //anim.SetBool("isWalking", true);
                     obroc_do_playera();
                     if (statyczny)
                     {
@@ -106,7 +106,7 @@ public class Enemy : MonoBehaviour
                 }
                 else
                 {
-                    anim.SetBool("isWalking", false);
+                    //anim.SetBool("isWalking", false);
                     if (time_tracker_powrotu + czas_na_powrot < Time.time)
                 {
                     if(punkty_powrotne.Count > 1)
