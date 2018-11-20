b1eb1d66ae16dad9b2bbb01a51d2b5b2b2cd4fb8
pablo <pawel.jeziorski.97@gmail.com>
2018-11-20 23:10:37 +0100

    cd

--- PGK_project/Assets/Scripts/EndLevel/NextLevelTest.cs
+++ PGK_project/Assets/Scripts/EndLevel/NextLevelTest.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class NextLevelTest : MonoBehaviour {
 
-    public void nextLevelButton()
+    public void nextLevelButton(string name)
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(name);
     }
 
 }
