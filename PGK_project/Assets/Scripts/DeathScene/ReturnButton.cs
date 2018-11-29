0b78e75760d8c29a462559a62e29ebf1929d08cc
pablo <pawel.jeziorski.97@gmail.com>
2018-11-29 07:35:27 +0100

    StarsRating

--- PGK_project/Assets/Scripts/DeathScene/ReturnButton.cs
+++ PGK_project/Assets/Scripts/DeathScene/ReturnButton.cs
@@ -7,6 +7,13 @@ using UnityEngine.UI;
 public class ReturnButton : MonoBehaviour {
     public void Return()
     {
+        DrugsStat.drugsAlcoValue = 0;
+        DrugsStat.drugsCocaValue = 0;
+        DrugsStat.drugsExtasyValue = 0;
+        DrugsStat.drugsHeraValue = 0;
+        DrugsStat.drugsMariValue = 0;
+        DrugsStat.drugsValue = 0;
+        KilledStat.killedValue = 0;
         ScoreCounter.scoreValue = 0;        //na deathscene widac pkt, po wcisnieciu return zerujemy
         SceneManager.LoadScene(1);
     }
---
+++ PGK_project/Assets/Scripts/Score/StarsScrpits.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsScrpits : MonoBehaviour {
+    public Timer timer;
+    public float totalPoints = 0;
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject star3;
+    public GameObject nextLevel;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        calcuulate();
+        if(totalPoints < 5)
+        {
+            nextLevel.SetActive(false);
+        }
+        else if(totalPoints >=5 && totalPoints <10)
+        {
+            star1.SetActive(true);
+        }
+        else if(totalPoints >= 10 && totalPoints < 15)
+        {
+            star1.SetActive(true);
+            star2.SetActive(true);
+        }
+        else if (totalPoints >= 15)
+        {
+            star1.SetActive(true);
+            star2.SetActive(true);
+            star3.SetActive(true);
+        }
+
+
+    }
+
+    public void calcuulate()
+    {
+        totalPoints = DrugsStat.drugsAlcoValue
+            + DrugsStat.drugsCocaValue
+            + DrugsStat.drugsExtasyValue
+            + (DrugsStat.drugsHeraValue * 3.2f)
+            + (DrugsStat.drugsMariValue * 1.5f)
+            + DrugsStat.drugsValue
+            + (KilledStat.killedValue * 0.1f);
+    }
+}
