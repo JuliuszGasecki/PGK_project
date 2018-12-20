52162790307ad90529e90df1a9065c61e5a40a68
Juliusz Gąsecki <julekg.97@gmail.com>
2018-12-20 13:16:02 +0100

    Małe zmiany :3

--- PGK_project/Assets/Scripts/Dialogue/DisplayDialogueBackground.cs
+++ PGK_project/Assets/Scripts/Dialogue/DisplayDialogueBackground.cs
@@ -7,6 +7,7 @@ public class DisplayDialogueBackground : MonoBehaviour {
 
     Text text;
     public GameObject image;
+    public GameObject image2;
 	// Use this for initialization
 	void Start () {
         text = gameObject.GetComponent<Text>();
@@ -15,8 +16,16 @@ public class DisplayDialogueBackground : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (text.text == null || text.text == "")
+        {
             image.SetActive(false);
+            image2.SetActive(false);
+        }
+
         else
+        {
             image.SetActive(true);
+            image2.SetActive(true);
+        }
+            
 	}
 }
