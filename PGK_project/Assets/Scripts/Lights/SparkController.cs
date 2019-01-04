fd54d48d59ed7a588094f15a8717369be2e2af68
Bartosz <bartekstep@gmail.com>
2019-01-04 23:47:07 +0100

    Sparks

---
+++ PGK_project/Assets/Scripts/Lights/SparkController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SparkController : MonoBehaviour
+{
+    private float _time;
+
+    private int _SparkPause;
+
+    private System.Random rng;
+	// Use this for initialization
+	void Start ()
+	{
+        rng = new System.Random();
+	    _time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _SparkPause = rng.Next(7, 10);
+	    if (Time.time > _time + _SparkPause)
+	    {
+	        gameObject.GetComponent<ParticleSystem>().Play();
+            _time = Time.time;
+	    }
+	}
+}
