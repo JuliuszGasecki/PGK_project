0597218ffa54b45442f9047f399c280dbed334c7
Juliusz Gąsecki <julekg.97@gmail.com>
2019-01-05 16:39:28 +0100

    Dodany efekt zjazddu po mecie

---
+++ PGK_project/Assets/Scripts/Drugs/DrugEffectsForPrefabs/MethEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MethEffect : MonoBehaviour {
+
+    private DrugsTimer drugsTimer;
+    public GameObject hangover;
+	// Use this for initialization
+	void Start () {
+        drugsTimer = GameObject.Find("Hero").GetComponent<DrugsTimer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!drugsTimer.methFlag)
+        {
+            Instantiate(hangover);
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ PGK_project/Assets/Scripts/Drugs/Mixed/Hangover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hangover : MonoBehaviour {
+
+    private float time;
+    private Hero hero;
+    private DrugsTimer drugsTimer;
+
+    private int maxHp;
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        hero = GameObject.Find("Hero").GetComponent<Hero>();
+        drugsTimer = GameObject.Find("Hero").GetComponent<DrugsTimer>();
+        time = Time.time;
+        maxHp = hero.maxHeath;
+        speed = hero.speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!drugsTimer.onDrugs)
+        {
+            hero.maxHeath = maxHp - 5;
+            hero.speed = speed - 5;
+        }
+        else
+        {
+            hero.maxHeath = maxHp;
+            hero.speed = speed;
+            Destroy(gameObject);
+        }
+		
+	}
+}
