ec6b264d8a78432c519a529f8410382de3e3598b
Juliusz Gąsecki <julekg.97@gmail.com>
2019-01-09 22:14:39 +0100

    To to w menu byczys :3

---
+++ PGK_project/Assets/Scripts/Menu/ChangeImage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour {
+
+    public List<Sprite> images;
+    public float timeToChange;
+    public Image image;
+    private float time;
+    private int currentIndex;
+	// Use this for initialization
+	void Start () {
+        time = Time.time;
+        currentIndex = 0;
+        image.sprite = images[currentIndex];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time - time > (currentIndex + 1) * timeToChange)
+        {
+            Debug.Log(currentIndex);
+            image.sprite = images[currentIndex];
+            currentIndex++;
+        }
+        indexOverflow();
+	}
+
+    private void indexOverflow()
+    {
+        if(currentIndex == images.Count)
+        {
+            currentIndex = 0;
+            time = Time.time;
+        }
+    }
+}
