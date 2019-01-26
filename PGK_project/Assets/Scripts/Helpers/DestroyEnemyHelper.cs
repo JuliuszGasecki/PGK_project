3d7c273e95863a34b066faea7aa1cd4ac93cb236
Juliusz Gąsecki <julekg.97@gmail.com>
2019-01-26 16:02:29 +0100

    Naprawione schody na disco + protytyp podświtelnia na disco

---
+++ PGK_project/Assets/Scripts/Helpers/DestroyEnemyHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemyHelper : MonoBehaviour {
+
+    public SpriteRenderer image;
+
+	// Update is called once per frame
+	void Update () {
+		if(this.gameObject.GetComponent<Enemy2>().life<=0)
+        {
+            image.enabled = false;
+        }
+	}
+}
