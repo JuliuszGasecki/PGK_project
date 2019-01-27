ddbdd01b20dd369b0894e03b2b1936b819242a6b
Juliusz Gąsecki <julekg.97@gmail.com>
2019-01-27 15:39:09 +0100

    Zrobione w menu żeby ostzreżenie o epilepsji i wieku wyswitlalo sie raz

---
+++ PGK_project/Assets/Scripts/Menu/TurnOffEnterance.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffEnterance : MonoBehaviour {
+
+    public static bool turnedOn = false;
+    public GameObject baby;
+
+	// Use this for initialization
+	void Start () {
+        if (turnedOn)
+        {
+            GameObject.Find("PEGI18").SetActive(false);
+            baby.SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ PGK_project/Assets/Scripts/Menu/setTurnOnMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setTurnOnMenu : MonoBehaviour {
+
+    public void turnOnMenu(int a)
+    {
+        TurnOffEnterance.turnedOn = true;
+    }
+}
